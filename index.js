054ec48b3ce8094fe66ea1c44888a9cbade3c0fb
unknown <vaibhavmishram3>
2024-06-25 22:07:49 +0530

    scroll bar

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+window.onscroll = function(){
+    scroll();
+};
+
+function scroll(){
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    var scrolled = (winScroll / height) * 100;
+
+    var a = document.querySelector(".progress-bar").style.width = scrolled + "%";
+    // console.log(winScroll);
+}
